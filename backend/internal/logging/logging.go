9feb00ddeb7e33b52ad9c5423d813828f8d08ec2
htchan <htchanbc@connect.ust.hk>
2022-03-19 10:27:48 +0800

    use json format in log data

--- backend/internal/logging/logging.go
+++ backend/internal/logging/logging.go
@@ -2,18 +2,34 @@ package logging
 
 import (
 	"log"
+	"encoding/json"
 )
 
 func Log(prefix string, data interface{}) {
-	log.Printf("web-history.%v %v", prefix, data)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("web-history.%v %v", prefix, data)
+	} else {
+		log.Printf("web-history.%v %v", prefix, string(jsonData))
+	}
 }
 
 func LogUpdate(url string, data interface{}) {
-	log.Printf("web-history.update.[%v] %v", url, data)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("web-history.update.[%v] %v", url, data)
+	} else {
+		log.Printf("web-history.update.[%v] %v", url, string(jsonData))
+	}
 }
 
 func LogRequest(action string, data map[string]interface{}) {
-	log.Printf("web-history.request.%v.%v", action, data)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("web-history.request.%v.%v", action, data)
+	} else {
+		log.Printf("web-history.request.%v.%v", action, string(jsonData))
+	}
 }
 
 func LogBatchStatus(status string) {
