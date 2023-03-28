f8033ddd1c6f3a3459368b8386fec088264b58b2
htchan <htchanbc@connect.ust.hk>
2023-03-28 21:59:49 +0800

    chore: update backup to schema.sql and data.sql

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -1,6 +1,13 @@
-tables=$(PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t -c "Select table_name From information_schema.tables Where table_type='BASE TABLE' and table_schema='public'")
-
 mkdir -p /backup/$(date '+%Y-%m-%d')
-for table in $tables;
-  do PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t $table > /backup/$(date '+%Y-%m-%d')/$table.sql;
-done;
+PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME --schema-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/schema.sql;
+PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME --data-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/data.sql;
+
+### backup by table ###
+# tables=$(PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t -c "Select table_name From information_schema.tables Where table_type='BASE TABLE' and table_schema='public'")
+
+# for table in $tables;
+#   do 
+#   PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t $table --schema-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/$table.schema.sql;
+#   PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t $table --data-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/$table.data.sql;
+# done;
+
