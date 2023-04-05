fef7a071462038625d61f0f12e968d50e2567496
htchan <htchanbc@connect.ust.hk>
2023-04-05 11:36:22 +0800

    feat: add backup cleaning to backup container

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -2,6 +2,9 @@ mkdir -p /backup/$(date '+%Y-%m-%d')
 PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME --schema-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/schema.sql;
 PGPASSWORD=$PSQL_PASSWORD pg_dump -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME --data-only | sed '/^SET\|^SELECT\|^-- Dumped/d' | sed '/^$/{N;/^\n$/d;}' > /backup/$(date '+%Y-%m-%d')/data.sql;
 
+# cleanup full backup to incremental backup
+python3 cleanup.py
+
 ### backup by table ###
 # tables=$(PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p $PSQL_PORT -U $PSQL_USER -d $PSQL_NAME -t -c "Select table_name From information_schema.tables Where table_type='BASE TABLE' and table_schema='public'")
 
